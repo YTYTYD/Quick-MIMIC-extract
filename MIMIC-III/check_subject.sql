17a66bc56b8633505bb62e10786e5b28a8e5db41
YTYTYD <68346601+YTYTYD@users.noreply.github.com>
2022-08-11 18:29:58 +1000

    Add files via upload

---
+++ MIMIC-III/check_subject.sql
@@ -0,0 +1,15 @@
+
+
+
+
+exec check_subject @HADM_ID='189535'
+
+alter proc check_subject (
+@HADM_ID  varchar(10)
+) 
+as
+SELECT  distinct [SUBJECT_ID]
+  FROM [mimic].[dbo].[DIAGNOSES_ICD] where HADM_ID = @HADM_ID
+
+--SELECT  distinct [SUBJECT_ID]
+--  FROM [mimic].[dbo].[DIAGNOSES_ICD] where HADM_ID = '165660'
